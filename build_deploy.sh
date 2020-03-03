bbb3b89ef2a4482e8ab38d2c14ec198fad3b6c8c
Andy Damevin <ia3andy@gmail.com>
2020-03-03 15:58:14 +0100

    Update build script

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-set -exv
+export IMAGE="quay.io/redhat-developer/code-quarkus"
 
-IMAGE="quay.io/redhat-developer/code-quarkus"
-IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-
-docker build -f Dockerfile.native.multistage -t "${IMAGE}:${IMAGE_TAG}" .
-
-if [[ -n "$QUAY_USER" && -n "$QUAY_TOKEN" ]]; then
-    DOCKER_CONF="$PWD/.docker"
-    mkdir -p "$DOCKER_CONF"
-    docker tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
-    docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-    docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
-    docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
-fi
+./code.quarkus.io/build_deploy.sh
