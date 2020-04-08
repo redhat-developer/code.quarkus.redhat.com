fa44e18f928c0597bdc0d68b7e5bffbedc77ce9e
Andy Damevin <ia3andy@gmail.com>
2020-04-08 11:28:07 +0200

    set NATIVE_BUILD_MEMORY=2g

--- build_deploy_int.sh
+++ build_deploy_int.sh
@@ -2,5 +2,6 @@
 
 export IMAGE="quay.io/app-sre/code-quarkus-int-build"
 export MAVEN_EXTRA_ARGS=" -Dquarkus.platform.group-id=com.redhat.quarkus -Dquarkus.version=${QUARKUS_VERSION-1.3.1.Final-redhat-00008} -Dquarkus.platform.version=${PLATFORM_VERSION-1.3.1.Final-redhat-00008} -Ptest-ga -s maven-settings.xml"
+export NATIVE_BUILD_MEMORY="2g"
 
 cd code.quarkus.io && ./build_deploy.sh
