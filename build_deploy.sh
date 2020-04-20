9bb40788f0bd3145a4120f6f32782814e2d803f0
Andy Damevin <ia3andy@gmail.com>
2020-04-20 15:28:22 +0200

    Release 1.3.2.Final-redhat-00001

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export IMAGE="quay.io/redhat-developer/code-quarkus"
+export MAVEN_EXTRA_ARGS="-s maven-settings.xml"
 
 cd code.quarkus.io && ./build_deploy.sh
--- build_deploy_int.sh
+++ build_deploy_int.sh
@@ -22,7 +22,7 @@ echo "Platform version: $PLATFORM_VERSION"
 ##GIT_REV=$(git rev-parse --short=7 HEAD)
 export IMAGE="quay.io/app-sre/code-quarkus-int-build"
 ##export IMAGE_TAG="$GIT_REV-q$QUARKUS_VERSION-p$PLATFORM_VERSION"
-export MAVEN_EXTRA_ARGS=" -Dquarkus.platform.group-id=com.redhat.quarkus -Dquarkus.version=${QUARKUS_VERSION-1.3.1.Final-redhat-00008} -Dquarkus.platform.version=${PLATFORM_VERSION-1.3.1.Final-redhat-00008} -Ptest-ga -s maven-settings.xml"
+export MAVEN_EXTRA_ARGS=" -Dquarkus.platform.group-id=com.redhat.quarkus -Dquarkus.version=${QUARKUS_VERSION-1.3.1.Final-redhat-00008} -Dquarkus.platform.version=${PLATFORM_VERSION-1.3.1.Final-redhat-00008} -Pinternal-build -s maven-settings.xml"
 export NATIVE_BUILD_MEMORY="2g"
 
 cd code.quarkus.io && ./build_deploy.sh
