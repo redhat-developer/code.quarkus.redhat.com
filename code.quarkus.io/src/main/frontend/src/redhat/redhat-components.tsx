78e39e10222200f9b0718f39bd56d98863825dde
Andy Damevin <ia3andy@gmail.com>
2020-05-11 16:00:22 +0200

    Fix layout after merge from upstream

--- code.quarkus.io/src/main/frontend/src/redhat/redhat-components.tsx
+++ code.quarkus.io/src/main/frontend/src/redhat/redhat-components.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import {WarningTriangleIcon} from "@patternfly/react-icons";
 import {ExternalLink} from "../core";
+import './redhat-components.scss';
 
 export function EnvConfNextStep(props: { onClick: (e: any) => void }) {
     const link = 'https://access.redhat.com/documentation/en-us/red_hat_build_of_quarkus/1.3/html/creating_quarkus_applications_with_apache_maven/con-apache-maven-plug-ins-and-quarkus_quarkus-maven';
     return (
-        <p>
+        <p className="env-config-next">
             <WarningTriangleIcon style={{color: 'red'}}/>&nbsp;You first need to <ExternalLink
                 href={link}
                 aria-label="Configure Red Hat online repository"
                 onClick={props.onClick}
-                style={{display: 'inline', whiteSpace: 'normal'}}
-            >configure your environment to use the Red Hat online repository</ExternalLink>.
+            >configure your environment</ExternalLink> to use the Red Hat online repository.
         </p>
     );
 }
