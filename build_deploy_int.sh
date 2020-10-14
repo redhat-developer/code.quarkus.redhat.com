ce7a822b50e7d805201e8958a9f7de2e8c110752
Andy Damevin <ia3andy@gmail.com>
2020-10-14 17:03:38 +0200

    Fix internal script

--- build_deploy_int.sh
+++ build_deploy_int.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-function findLatestBuild() {
-  cat internal-build-version.txt
-}
+BUILD_VERSION="$(cat internal-build-version.txt)"
+echo "Build version: $BUILD_VERSION"
+[[ -z "$BUILD_VERSION" ]] && { echo "BUILD_VERSION is empty" ; exit 1; }
 
 function findLatestQuarkusVersion() {
-  QUARKUS_VERSION="$(findLatestBuild)"
-  LIST_HTML="$(curl -s "http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/quarkus/$QUARKUS_VERSION/extras/repository-artifact-list.txt")"
+  LIST_HTML="$(curl -s "http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/quarkus/$BUILD_VERSION/extras/repository-artifact-list.txt")"
   REGEX="($1):([^:]+):([a-zA-Z0-9.-]+)"
   [[ $LIST_HTML =~ $REGEX ]] && echo "${BASH_REMATCH[3]}"
-}
 
 QUARKUS_VERSION="$(findLatestQuarkusVersion "io.quarkus")"
 PLATFORM_VERSION="$(findLatestQuarkusVersion "com.redhat.quarkus")"
@@ -19,7 +17,7 @@ echo "Platform version: $PLATFORM_VERSION"
 GIT_REV=$(git rev-parse --short=7 HEAD)
 export IMAGE="quay.io/app-sre/code-quarkus-int-build"
 
-export MAVEN_EXTRA_ARGS=" -Dquarkus.version=${QUARKUS_VERSION-1.3.1.Final-redhat-00008} -Dquarkus.platform.version=${PLATFORM_VERSION-1.3.1.Final-redhat-00008} -Pinternal-build -s maven-settings.xml"
+export MAVEN_EXTRA_ARGS=" -Dquarkus.version=${QUARKUS_VERSION} -Dquarkus.platform.version=${PLATFORM_VERSION} -Pinternal-build -s maven-settings.xml"
 export NATIVE_BUILD_MEMORY="3g"
 
 cd code.quarkus.io && ./build_deploy.sh
