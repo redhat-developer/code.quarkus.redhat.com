607736eff2f1cb8a26c2d2eb85a82c59429a6cf4
Alexey Loubyansky <olubyans@redhat.com>
2023-08-14 15:15:55 +0200

    Added dev-preview support scope

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -36,6 +36,10 @@ const tagsDef = [
         href: 'https://access.redhat.com/support/offerings/production/soc/',
         color: '#6AB983'
     },
+        name: 'dev-preview',
+        href: 'https://access.redhat.com/support/offerings/devpreview',
+        color: '#6AB983'
+    },
     {
         name: 'tech-preview',
         description: 'Technology Preview features provide early access to upcoming product innovations, enabling you to test functionality and provide feedback during the development process. However, these features are not fully supported under Red Hat Subscription Level Agreements.',
@@ -59,6 +63,11 @@ const tagsDef = [
     {
         name: 'redhat-support:dev-support',
         href: 'https://access.redhat.com/support/offerings/production/soc/',
+        color: '#6AB983'
+    },
+    {
+        name: 'redhat-support:dev-preview',
+        href: 'https://access.redhat.com/support/offerings/devpreview',
         color: '#6AB983'
     },
     {
