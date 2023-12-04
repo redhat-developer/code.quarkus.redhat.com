5815195ee0fe4637793b57c5112ed2efc1e4f8de
Andy Damevin <ia3andy@gmail.com>
2023-12-04 15:57:13 +0100

    Switch to Web Bundler

--- redhat-app/src/main/resources/web/redhat-app/index.jsx
+++ redhat-app/src/main/resources/web/redhat-app/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'react-app-polyfill/ie11';
-import reportWebVitals from './reportWebVitals';
-import {CodeQuarkus, fetchConfig, fetchPlatform} from '@quarkusio/code-quarkus.components';
+import {CodeQuarkus, fetchConfig, fetchPlatform} from '../lib';
 import './theme.scss';
 import {RedHatHeader} from './header/redhat-header';
 
-const PUBLIC_URL = process.env.PUBLIC_URL && `${process.env.PUBLIC_URL}/`;
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || PUBLIC_URL;
+const PUBLIC_URL = window.PUBLIC_URL;
 const CLIENT_NAME = window.location.hostname;
 const REQUEST_OPTIONS = {headers: {'Client-Name': CLIENT_NAME}};
 
@@ -37,11 +35,6 @@ const tagsDef = [
         color: '#6AB983'
     },
     {
-        name: 'dev-preview',
-        href: 'https://access.redhat.com/support/offerings/devpreview',
-        color: '#6AB983'
-    },
-    {
         name: 'tech-preview',
         description: 'Technology Preview features provide early access to upcoming product innovations, enabling you to test functionality and provide feedback during the development process. However, these features are not fully supported under Red Hat Subscription Level Agreements.',
         color: '#4A97E8'
@@ -64,11 +57,6 @@ const tagsDef = [
     {
         name: 'redhat-support:dev-support',
         href: 'https://access.redhat.com/support/offerings/production/soc/',
-        color: '#6AB983'
-    },
-    {
-        name: 'redhat-support:dev-preview',
-        href: 'https://access.redhat.com/support/offerings/devpreview',
         color: '#6AB983'
     },
     {
@@ -109,20 +97,16 @@ const tagsDef = [
 
 
 const api = {
-    backendUrl: BACKEND_URL,
+    backendUrl: PUBLIC_URL,
     clientName: CLIENT_NAME,
     requestOptions: REQUEST_OPTIONS,
     tagsDef
 };
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
     <React.StrictMode>
         <CodeQuarkus api={api} configApi={fetchConfig} platformApi={fetchPlatform} header={RedHatHeader}/>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
