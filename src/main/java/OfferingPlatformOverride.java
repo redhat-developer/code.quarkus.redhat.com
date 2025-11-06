3455cfe99233a2010c1da4e2b8fcafdafdc1b488
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2025-11-06 15:27:42 +0100

    feat: Add custom preset for redhat-camel variant

--- src/main/java/OfferingPlatformOverride.java
+++ src/main/java/OfferingPlatformOverride.java
@@ -13,6 +13,9 @@ import java.util.function.Function;
 @Singleton
 public class OfferingPlatformOverride implements PlatformOverride {
 
+
+    public static final String ICONS_ASSETS_URL = "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons";
+
     public static final List<Preset> IBM_PRESETS = List.of(
             // Some presets are duplicated to support platforms before and after the Big Reactive Renaming
             new Preset("rest-service", "REST service",
@@ -43,6 +46,38 @@ public class OfferingPlatformOverride implements PlatformOverride {
                     List.of("io.quarkiverse.langchain4j:quarkus-langchain4j-openai",
                             "io.quarkiverse.langchain4j:quarkus-langchain4j-easy-rag")));
 
+    public static final List<Preset> REDHAT_CAMEL_PRESETS = List.of(
+            // Some presets are duplicated to support platforms before and after the Big Reactive Renaming
+            new Preset("web-service", "Web service",
+                    ICONS_ASSETS_URL + "/redhat-camel-presets/orange-rest_pos.svg",
+                    List.of("org.apache.camel.quarkus:camel-quarkus-rest",
+                            "org.apache.camel.quarkus:camel-quarkus-rest-openapi",
+                            "org.apache.camel.quarkus:camel-quarkus-cxf-soap",
+                            "io.quarkiverse.cxf:quarkus-cxf")),
+            new Preset("db-service", "REST service with database",
+                    ICONS_ASSETS_URL + "/redhat-camel-presets/orange-db-service_pos.svg",
+                    List.of("org.apache.camel.quarkus:camel-quarkus-rest",
+                            "org.apache.camel.quarkus:camel-quarkus-aws2-ddb",
+                            "org.apache.camel.quarkus:camel-quarkus-cassandraql",
+                            "org.apache.camel.quarkus:camel-quarkus-jdbc",
+                            "org.apache.camel.quarkus:camel-quarkus-jpa",
+                            "org.apache.camel.quarkus:camel-quarkus-kudu",
+                            "org.apache.camel.quarkus:camel-quarkus-ldap",
+                            "org.apache.camel.quarkus:camel-quarkus-mongodb",
+                            "org.apache.camel.quarkus:camel-quarkus-mybatis",
+                            "org.apache.camel.quarkus:camel-quarkus-sql")),
+            new Preset("event-driven-kafka", "Event driven service with Kafka",
+                    ICONS_ASSETS_URL + "/redhat-camel-presets/orange-event-driven-kafka_pos.svg",
+                    List.of("org.apache.camel.quarkus:camel-quarkus-kafka",
+                            "org.apache.camel.quarkus:camel-quarkus-jms")),
+            new Preset("ai-infused", "AI Infused service",
+                    ICONS_ASSETS_URL + "/redhat-camel-presets/orange-ai-infused_pos.svg",
+                    List.of("org.apache.camel.quarkus:camel-quarkus-langchain4j-agent",
+                            "org.apache.camel.quarkus:camel-quarkus-langchain4j-chat",
+                            "org.apache.camel.quarkus:camel-quarkus-langchain4j-tokenizer",
+                            "org.apache.camel.quarkus:camel-quarkus-langchain4j-tools",
+                            "org.apache.camel.quarkus:camel-quarkus-langchain4j-web-search")));
+
     @Inject
     OfferingConfig config;
 
@@ -61,6 +96,8 @@ public class OfferingPlatformOverride implements PlatformOverride {
         switch (config.id()) {
             case "ibm":
                 return IBM_PRESETS;
+            case "redhat-camel":
+                return REDHAT_CAMEL_PRESETS;
             default:
                 return PlatformService.DEFAULT_PRESETS.stream()
                         .map(p -> new Preset(p.key(), p.title(), p.icon().replace("_neg", "_pos"), p.extensions()))
