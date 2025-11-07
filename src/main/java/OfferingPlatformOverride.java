e86ef94cbf4dcf9394a36c7f3fa91db7ff5537e1
Gaëlle Fournier <gaelle.fournier.work@gmail.com>
2025-11-07 09:51:17 +0100

    feat: Add custom preset for redhat-camel variant (#207)
    * feat: Add custom preset for redhat-camel variant
    
    * refactor: IBM preset image URL

--- src/main/java/OfferingPlatformOverride.java
+++ src/main/java/OfferingPlatformOverride.java
@@ -13,35 +13,70 @@ import java.util.function.Function;
 @Singleton
 public class OfferingPlatformOverride implements PlatformOverride {
 
+
+    public static final String ICONS_ASSETS_URL = "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons";
+
     public static final List<Preset> IBM_PRESETS = List.of(
             // Some presets are duplicated to support platforms before and after the Big Reactive Renaming
             new Preset("rest-service", "REST service",
-                    "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons/ibm-presets/rest-service.svg",
+                    ICONS_ASSETS_URL + "/ibm-presets/rest-service.svg",
                     List.of("io.quarkus:quarkus-rest")),
             new Preset("db-service", "REST service with database",
-                    "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons/ibm-presets/db-service.svg",
+                    ICONS_ASSETS_URL + "/ibm-presets/db-service.svg",
                     List.of("io.quarkus:quarkus-rest", "io.quarkus:quarkus-rest-jackson",
                             "io.quarkus:quarkus-hibernate-orm-panache", "io.quarkus:quarkus-jdbc-postgresql")),
             new Preset("event-driven-kafka", "Event driven service with Kafka",
-                    "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons/ibm-presets/event-driven.svg",
+                    ICONS_ASSETS_URL + "/ibm-presets/event-driven.svg",
                     List.of("io.quarkus:quarkus-messaging-kafka")),
             new Preset("cli", "Command-line tool",
-                    "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons/ibm-presets/cli.svg",
+                    ICONS_ASSETS_URL + "/ibm-presets/cli.svg",
                     List.of("io.quarkus:quarkus-picocli")),
             new Preset("webapp-mvc", "Web app with Model-View-Controller",
-                    "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons/ibm-presets/webapp-mvc.svg",
+                    ICONS_ASSETS_URL + "/ibm-presets/webapp-mvc.svg",
                     List.of("io.quarkiverse.renarde:quarkus-renarde", "io.quarkiverse.web-bundler:quarkus-web-bundler")),
             new Preset("webapp-npm", "Web app with NPM UI",
-                    "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons/ibm-presets/webapp-npm.svg",
+                    ICONS_ASSETS_URL + "/ibm-presets/webapp-npm.svg",
                     List.of("io.quarkus:quarkus-rest", "io.quarkus:quarkus-rest-jackson",
                             "io.quarkiverse.quinoa:quarkus-quinoa")),
             new Preset("webapp-qute", "Web app with ServerSide Rendering",
-                    "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons/ibm-presets/webapp-qute.svg",
+                    ICONS_ASSETS_URL + "/ibm-presets/webapp-qute.svg",
                     List.of("io.quarkiverse.qute.web:quarkus-qute-web", "io.quarkiverse.web-bundler:quarkus-web-bundler")),
             new Preset("ai-infused", "AI Infused service",
-                    "https://raw.githubusercontent.com/redhat-developer/code.quarkus.redhat.com/refs/heads/main/assets/icons/ibm-presets/ai-infused.svg",
+                    ICONS_ASSETS_URL + "/ibm-presets/ai-infused.svg",
                     List.of("io.quarkiverse.langchain4j:quarkus-langchain4j-openai",
                             "io.quarkiverse.langchain4j:quarkus-langchain4j-easy-rag")));
+
+    public static final List<Preset> REDHAT_CAMEL_PRESETS = List.of(
+            // Some presets are duplicated to support platforms before and after the Big Reactive Renaming
+            new Preset("web-service", "Web service",
+                    ICONS_ASSETS_URL + "/redhat-camel-presets/orange-rest_pos.svg",
+                    List.of("org.apache.camel.quarkus:camel-quarkus-rest",
+                            "org.apache.camel.quarkus:camel-quarkus-rest-openapi",
+                            "org.apache.camel.quarkus:camel-quarkus-cxf-soap",
+                            "io.quarkiverse.cxf:quarkus-cxf")),
+            new Preset("db-service", "REST service with database",
+                    ICONS_ASSETS_URL + "/redhat-camel-presets/orange-db-service_pos.svg",
+                    List.of("org.apache.camel.quarkus:camel-quarkus-rest",
+                            "org.apache.camel.quarkus:camel-quarkus-aws2-ddb",
+                            "org.apache.camel.quarkus:camel-quarkus-cassandraql",
+                            "org.apache.camel.quarkus:camel-quarkus-jdbc",
+                            "org.apache.camel.quarkus:camel-quarkus-jpa",
+                            "org.apache.camel.quarkus:camel-quarkus-kudu",
+                            "org.apache.camel.quarkus:camel-quarkus-ldap",
+                            "org.apache.camel.quarkus:camel-quarkus-mongodb",
+                            "org.apache.camel.quarkus:camel-quarkus-mybatis",
+                            "org.apache.camel.quarkus:camel-quarkus-sql")),
+            new Preset("event-driven-kafka", "Event driven service with Kafka",
+                    ICONS_ASSETS_URL + "/redhat-camel-presets/orange-event-driven-kafka_pos.svg",
+                    List.of("org.apache.camel.quarkus:camel-quarkus-kafka",
+                            "org.apache.camel.quarkus:camel-quarkus-jms")),
+            new Preset("ai-infused", "AI Infused service",
+                    ICONS_ASSETS_URL + "/redhat-camel-presets/orange-ai-infused_pos.svg",
+                    List.of("org.apache.camel.quarkus:camel-quarkus-langchain4j-agent",
+                            "org.apache.camel.quarkus:camel-quarkus-langchain4j-chat",
+                            "org.apache.camel.quarkus:camel-quarkus-langchain4j-tokenizer",
+                            "org.apache.camel.quarkus:camel-quarkus-langchain4j-tools",
+                            "org.apache.camel.quarkus:camel-quarkus-langchain4j-web-search")));
 
     @Inject
     OfferingConfig config;
@@ -61,6 +96,8 @@ public class OfferingPlatformOverride implements PlatformOverride {
         switch (config.id()) {
             case "ibm":
                 return IBM_PRESETS;
+            case "redhat-camel":
+                return REDHAT_CAMEL_PRESETS;
             default:
                 return PlatformService.DEFAULT_PRESETS.stream()
                         .map(p -> new Preset(p.key(), p.title(), p.icon().replace("_neg", "_pos"), p.extensions()))
